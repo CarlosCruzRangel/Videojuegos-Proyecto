b05be32958a6646e2259275a01cbad5762832445
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-11-22 20:18:22 -0600

    Pantalla Manual
    Se agrego la pantalla del instructivo

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MenuInicial : MonoBehaviour
 {
     // Start is called before the first frame update
     public void Iniciar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     // Update is called once per frame
     public void Manual()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
