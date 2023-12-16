1584fb183cc7b585311b10b172d74e610e1dcfde
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-12-15 21:48:21 -0600

    Update MenuInicial.cs

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -8,12 +8,12 @@ public class MenuInicial : MonoBehaviour
     // Start is called before the first frame update
     public void Iniciar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene("Level1");
     }
 
     // Update is called once per frame
     public void Manual()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Manual");
     }
 }
